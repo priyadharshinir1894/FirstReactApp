e82984ad18b9bc0ce89f714e1715661470e611af
Prajaram <prajaram345@gmail.com>
2019-09-11 23:49:16 +0100

    Created a server.

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({
+    Name: "Priya"
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server listening at port 3000");
+});
