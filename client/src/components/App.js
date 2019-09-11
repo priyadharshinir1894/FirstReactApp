b45ef31024298dceea741ce569a6806610ce6b36
Prajaram <prajaram345@gmail.com>
2019-09-12 00:07:06 +0100

    Simple State Update.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from "react";
 
 class App extends Component {
+  state = {
+    Name: null
+  };
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ Name: "Priya" });
+    }, 1000);
+  }
   render() {
-    return <div className="App">Hi</div>;
+    return (
+      <div className="App">
+        {this.state.Name ? "Hello, " + this.state.Name : "Loading..."}
+      </div>
+    );
   }
 }
 
